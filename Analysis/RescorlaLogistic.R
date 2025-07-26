18d42b404e5c98b07aec5f02f5b53c3a63254cf0
znhoughton <znhoughton@gmail.com>
2025-07-26 02:06:47 -0700

    updated analysis

--- Analysis/RescorlaLogistic.R
+++ Analysis/RescorlaLogistic.R
@@ -394,3 +394,64 @@ BushMosteller = function (cuesOutcomes,
   }
   return(w)
 }
+
+
+Rescorla_modified_alpha_for_semantic_cues = function (cuesOutcomes,  
+                     Lambda = 2, 
+                     Alpha = 0.05,
+                     SemAlpha = 0.25,
+                     Beta = 0.1,logistic=FALSE,sigSlope=1) 
+{
+  cues = unique(unlist(strsplit(cuesOutcomes$Cues, "_")))
+  outcomes = unique(unlist(strsplit(cuesOutcomes$Outcomes, "_")))
+  frequency = cuesOutcomes$Frequency
+  w = matrix(0, length(cues), length(outcomes))
+  rownames(w)=cues
+  colnames(w)=outcomes
+  theCues = strsplit(cuesOutcomes$Cues, "_")
+  theOutcomes = strsplit(cuesOutcomes$Outcomes, "_")
+  rate=Alpha*Beta
+  semrate = SemAlpha*Beta
+  
+  # new bit
+  knownOutcomes = NULL
+  x=-0.5
+  
+  for (i in 1:nrow(cuesOutcomes)) {
+    cs = theCues[[i]]
+    ou = theOutcomes[[i]] 
+    
+    # new two lines
+    toAdd = ou[!is.element(ou, knownOutcomes)]
+    knownOutcomes = c(knownOutcomes, toAdd)
+    
+    if (logistic==FALSE) {Vtotal = colSums(w[cs,ou,drop=FALSE])}
+    else {Vtotal = 1/(1+exp((-1)*sigSlope*colSums(w[cs,ou,drop=FALSE])))}      
+    
+    cue_names = rownames(w[cs,ou, drop=F])
+    rates = ifelse(cue_names %in% semantic_cues, semrate, rate)
+    
+    w[cs,ou] = t(t(w[cs,ou]) + (Lambda-Vtotal)*rates*frequency[i])
+    
+    # replaced next line by new line
+    # otherou = outcomes[!is.element(outcomes,ou)]
+    otherou = knownOutcomes[!is.element(knownOutcomes, ou)]
+    # this next line may not work, in which case check if (length(otherou)>0)
+    if (!is.null(otherou)) {
+      if (logistic==FALSE) {Vtotal = colSums(w[cs,otherou,drop=FALSE])}
+      else {Vtotal = 1/(1+exp((-1)*sigSlope*colSums(w[cs,otherou,drop=FALSE])))}
+      #1/(1+exp((-1)*sigSlope*sum(weightvec[currentCues])))
+      
+      otherou_cue_names = rownames(w[cs,otherou, drop=F])
+      rates_otherou = ifelse(otherou_cue_names %in% semantic_cues, semrate, rate)
+      adj_matrix = matrix(rep((0 - Vtotal) * frequency[i], each=length(cs)),
+                          nrow=length(cs), byrow=FALSE)
+      
+      # Multiply each row by its rate using sweep
+      adj_matrix = sweep(adj_matrix, 1, rates_otherou, "*")
+      w[cs,otherou] = t(t(w[cs,otherou])) + adj_matrix
+      
+    }
+  }
+  return(w)
+}
